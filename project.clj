c75db7c8719b7731ac4d02c0bcfa3ba275dd9b05
Ben Ashford <ben@bcash.org>
2015-02-21 12:52:55 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject redis-async "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [aleph "0.4.0-beta3"]])
