c60cb9e2ddacff79f8664b1d50d821dcbd8323f9
Ben Ashford <benashford@users.noreply.github.com>
2015-08-15 20:29:18 +0100

    Dependency updates

--- project.clj
+++ project.clj
@@ -5,10 +5,9 @@
             :url          "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
   :java-source-paths ["jresp/src/main/java"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [io.netty/netty-all "4.0.28.Final"]
-                 [cheshire "5.4.0"]]
+                 [cheshire "5.5.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]]
                    :jvm-opts ^:replace []}}
   :aliases {"bench-repl" ["with-profile" "benchmark" "repl" ":headless"]})
