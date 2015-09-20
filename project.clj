af67b0830991756206e424aeac88cb84ccbdb7b2
Ben Ashford <benashford@users.noreply.github.com>
2015-09-20 18:48:54 +0100

    Next version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject redis-async "0.3.0"
+(defproject redis-async "0.3.1"
   :description "An asynchronous Redis client"
   :url "https://github.com/benashford/redis-async"
   :license {:name         "Apache Licence 2.0"
