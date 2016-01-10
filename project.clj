7b3866b9bba405da49f79ed8fe2c4a610152c29c
Ben Ashford <benashford@users.noreply.github.com>
2016-01-10 20:27:35 +0000

    Version bumps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject redis-async "0.3.2"
+(defproject redis-async "0.3.3-SNAPSHOT"
   :description "An asynchronous Redis client"
   :url "https://github.com/benashford/redis-async"
   :license {:name         "Apache Licence 2.0"
@@ -6,7 +6,7 @@
             :distribution :repo}
   :java-source-paths ["jresp/src/main/java"]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.374"]
                  [cheshire "5.5.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]]
                    :jvm-opts ^:replace []}}
